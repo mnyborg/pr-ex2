3fbcca721f54744d9e4af53f815a62b84d5c91e0
mnyborg <nyborg62@gmail.com>
2025-02-16 18:58:36 +0100

    Initial commit

---
+++ src/main/java/org/example/prex2/PrEx2Application.java
@@ -0,0 +1,13 @@
+package org.example.prex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrEx2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrEx2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prex2/PrEx2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrEx2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
